fb928eaee4c7e026442137e5bcec652a63bf5f45
ThiagoThe <nitrox2thi@gmail.com>
2022-12-18 15:22:18 -0300

    Atualizando com novas funções

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     divListagem.style.display = "none";
     divCadastro.style.display = "block";
   };
+
+  // Submissão do form
+  const frmCadastro = document.getElementById("frmCadastro");
+  frmCadastro.onsubmit = (evt) => {
+    let linha = "<tr>";
+    var data = new FormData(frmCadastro);
+    for (let item of data) linha += `<td>${item[1]}</td>`;
+    linha += `<td><input type="button" value="X" /></td></tr>`;
+
+    //se tem apenas uma TD, é a default
+    const tbody = document.querySelector("table>tbody");
+    if (tbody.querySelectorAll("tr > td").length === 1) tbody.innerHTML = "";
+
+    tbody.innerHTML += linha;
+
+    divListagem.style.display = "block";
+    divCadastro.style.display = "none";
+
+    frmCadastro.reset();
+
+    // Exclusão
+    const buttons = document.querySelectorAll("input[value='X']");
+    for (let btn of buttons) {
+      if (btn.onclick !== null) continue;
+      btn.onclick = (evt) => {
+        if (confirm("Você tem certeza que quer excluir este cliente?")) {
+          btn.closest("tr").remove();
+        }
+      };
+    }
+
+    evt.preventDefault();
+  };
 });
